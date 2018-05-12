37b000b01b063ec2456479b1d9388f253f3174a3
Pruvost Geoffrey <geoffrey.pruvost@hotmail.com>
2018-05-12 21:56:48 +0200

    full integration of FOSUser (routes, design, ...)

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -10,11 +10,12 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
+        return $this->render('@App/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
--- src/UserBundle/Controller/DefaultController.php
+++ src/UserBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ use UserBundle\Form\UserType;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="user_index")
+     * @Route("/", name="account_index")
      * @return \Symfony\Component\HttpFoundation\Response
      * @Security("has_role('ROLE_USER')")
      */
@@ -21,35 +21,5 @@ class DefaultController extends Controller
         return $this->render('@User/Default/index.html.twig');
     }
 
-    /**
-     * @Route("/create")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     *
-     */
-    public function addAction(Request $request)
-    {
-        $user = new User();
-
-        $form = $this->createForm(UserType::class, $user);
-
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-
-            $em = $this->getDoctrine()->getManager();
-            $user->setSalt(date("Y-m-d_H:i:s"));
-            $user->setRoles(array('ROLE_USER'));
-            $em->persist($user);
-            $em->flush();
-
-            $request->getSession()->getFlashBag()->add('notice', 'Votre compte est correctement créé');
-
-            return $this->redirectToRoute('user_index', array('id' => $user->getId()));
-
-        }
-
-        return $this->render('@User/Default/inscription.html.twig', array(
-            'form' => $form->createView(),
-        ));
-    }
 }
 
